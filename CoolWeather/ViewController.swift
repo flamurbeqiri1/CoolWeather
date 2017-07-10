6955ab1935ec7190bd3672e73780cd62b9871090
Flamur <flamurbeqiri1@gmail.com>
2017-07-10 21:56:28 +0200

    Initial Commit

---
+++ CoolWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolWeather
+//
+//  Created by Flamur on 7/10/17.
+//  Copyright © 2017 Codeators. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
