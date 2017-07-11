3f05e8917786b21c4ed1f9d95ebdbbb414517f16
Flamur <flamurbeqiri1@gmail.com>
2017-07-11 15:40:38 +0200

    Done with UI, tableView implemented etc.

--- CoolWeather/WeatherVC.swift
+++ CoolWeather/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  CoolWeather
 //
 //  Created by Flamur on 7/10/17.
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var currentTempLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var currentWeatherImage: UIImageView!
+    @IBOutlet var currentWeatherLabel: UILabel!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
     }
 
 
